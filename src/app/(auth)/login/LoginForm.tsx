301c660439208b1869f9b3f5bf781105bbc1dfc7
Jack <jack282323@gmail.com>
2024-12-17 22:10:50 +0100

    Fixed - working on pusher and auth redirect

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -42,7 +42,7 @@ export default function LoginForm() {
             <GiPadlock size={30} />
             <h1 className="text-3xl font-semibold">Login</h1>
           </div>
-          <p className="text-neutral-500">Welcome back to MatchMe!</p>
+          <p className="text-neutral-500">Welcome back, Supaluva!</p>
         </div>
       </CardHeader>
       <CardBody>
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -16,12 +16,16 @@ export default {
       async authorize(creds) {
         const validated = loginSchema.safeParse(creds);
 
-        if (validatedSuccess) {
+        if (validated.success) {
           const { email, password } = validated.data;
 
           const user = await getUserByEmail(email);
 
-          if (!user || !(await compare(password, user.passwordHash)))
+          if (
+            !user ||
+            !user.passwordHash ||
+            !(await compare(password, user.passwordHash))
+          )
             return null;
 
           return user;
